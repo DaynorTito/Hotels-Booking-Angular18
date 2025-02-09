85ee061924ff660549c46a03066ada56d1e555f6
Daynor Tito Condori <daynortito13@gmail.com>
2025-02-09 05:15:22 -0400

    feat: login register

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,14 +1,56 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FooterComponent } from "../../../shared/footer/footer.component";
-import { HeaderComponent } from "../../../shared/header/header.component";
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Home, LucideAngularModule, EyeOff, Eye} from 'lucide-angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FooterComponent, HeaderComponent],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    LucideAngularModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  showPassword: boolean = false;
+  readonly EyeIcon = Eye;
+  readonly EyeCloseIcon = EyeOff;
+  readonly HomeIcon = Home;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  isFieldInvalid(fieldName: string): boolean {
+    const field = this.loginForm.get(fieldName);
+    return field ? field.invalid && (field.dirty || field.touched) : false;
+  }
+
+  validateErrorField(fieldName: string, rule: string): boolean {
+    const field = this.loginForm.get(fieldName);
+    return field?.errors?.[rule];
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
 }
